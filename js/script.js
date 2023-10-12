5dd4f765015908b26c4f999af816bf76f8cebba1
basithahamed <basithhahamed@gmail.com>
2023-10-12 23:05:50 +0530

    initialcommit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function toogleNav()
+{
+    var x = document.getElementById("mobile-links");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+
+}
